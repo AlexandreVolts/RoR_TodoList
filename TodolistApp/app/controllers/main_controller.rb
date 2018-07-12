20ac934b30e6901d098997f06b4edf40640095b5
alexandre.cochet <alexandre.cochet@localhost.localdomain>
2018-07-12 16:21:02 +0200

    User can now delete elements. A lot of CSS added.^C

--- TodolistApp/app/controllers/main_controller.rb
+++ TodolistApp/app/controllers/main_controller.rb
@@ -2,23 +2,51 @@ class MainController < ApplicationController
   def home
     @todos = Todo.all
   end
+  
   def error
     if params[:error] == "1"
-      @error = "Can't find selected todo element."
+      @error = "Can't find selected element."
+    elsif params[:error] == "2"
+      @error = "An input contains an invalid number of characters."
+    elsif params[:error] == "404"
+      @error = "WELCOME TO THE LEGENDARY, THE GORGEOUS, THE ONLY ONE 404 ERROR."
     else
       @error = "An unknow error occured."
     end
   end
+  
   def add
+    if params[:content].length < 5 || params[:content].length > 50
+      redirect_to("/error/2")
+      return
+    end
+    if params[:content].length < 2 || params[:content].length > 20
+      redirect_to("/error/2")
+      return
+    end
     Todo.create(content: params[:content],
                 date: DateTime.now,
                 author: params[:author])
     redirect_to("/")
   end
+  
   def show
     @todo = Todo.find_by(id: params[:id])
     if @todo == nil
       redirect_to("/error/1")
     end
+  end
+  
+  def delete
+    @todo = Todo.find_by(id: params[:id])
+    if @todo == nil
+      redirect_to("/error/1")
+    end
+    @todo.destroy()
+    redirect_to("/")
+  end
+
+  def not_found
+    redirect_to("/error/404")
   end
 end
--- TodolistApp/config/routes.rb
+++ TodolistApp/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'main#home'
   get '/todo/:id' => 'main#show'
   get '/error/:error' => 'main#error'
+  get '*unmatched_route', to: 'main#not_found'
+  delete '/todo/:id' => 'main#delete'
   post '/' => 'main#add'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
